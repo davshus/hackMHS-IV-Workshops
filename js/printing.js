82aa76138669d7e70619c32bcbe8a2fd1cd1fe62
David Shustin <david.shustin@gmail.com>
2017-05-07 18:50:04 -0400

    Create variables and printing

---
+++ js/printing.js
@@ -0,0 +1,3 @@
+console.log("Hello!");
+console.log("I hope you're having fun at hackMHS IV!", "We worked a lot to make it great!");
+console.log(5);
---
+++ js/variables.js
@@ -0,0 +1,12 @@
+var number = 5;
+var string = "Hello, world!";
+var boolean = true;
+var array = [1, 2, 3, 4, 5];
+var object = {
+  item: "This is an item!",
+  numitem: 9001,
+  arritem: [5, 4, 3, 2, 1],
+  objectitem: {
+    info: "This is an object inside of an object!"
+  }
+};
