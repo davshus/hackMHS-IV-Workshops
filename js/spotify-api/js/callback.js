7ca45599e3dbd79bff0637f21e611f100d500468
David Shustin <david.shustin@gmail.com>
2017-05-26 21:07:45 -0400

    Finish spotify API app

--- js/spotify-api/js/callback.js
+++ js/spotify-api/js/callback.js
@@ -1,2 +1,42 @@
-var params = window.location.hash.substring(1);
-console.log(params);
+const params = window.location.hash.substring(1);
+const token = $.deparam(params).access_token;
+$.ajax({
+  url: 'https://api.spotify.com/v1/me/',
+  type: 'GET',
+  data: {
+    format: 'json'
+  },
+  headers: {
+    'Authorization': 'Bearer ' + token
+  },
+  success: function (data) {
+    $('#header-container').append('<br /><br /><h1 id=\'welcome\'>Hello, ' + (!data.display_name ? data.id : data.display_name) + '!</h1>');
+  },
+  error: function() {
+    console.error('An error occurred in getting user information.');
+  }
+});
+$.ajax({
+  url: 'https://api.spotify.com/v1/me/tracks',
+  type: 'get',
+  data: {
+    format: 'json'
+  },
+  headers: {
+    'Authorization': 'Bearer ' + token
+  },
+  success: function(data) {
+    data.items.forEach((val, ind, arr) => addCard(val.track.name, val.track.artists.map(x => x.name).join(', ')));
+  },
+  error: function() {
+    console.error('An error occurred in getting user tracklist.')
+  }
+})
+function addCard(title, author) {
+  $('#body-container').append('\
+  <div class=\'card\'>\
+    <span class=\'track-title\'>' + title + '</span><br />\
+    <span class=\'track-author\'>' + author + '</span>\
+  </div>\
+  ')
+}
